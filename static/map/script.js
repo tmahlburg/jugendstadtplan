cf8326f175c484f8cf5389e1e6b85381db8a9cba
Till Mahlburg <mahlburg@posteo.de>
2020-03-27 19:11:47 +0100

    rename stadtplan to map

--- static/map/script.js
+++ static/map/script.js
@@ -1,5 +1,5 @@
 
-var map = L.map('stadtplan', {zoomControl: false}).setView([54.095166, 13.3710154], 17);
+var map = L.map('map', {zoomControl: false}).setView([54.095166, 13.3710154], 17);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
