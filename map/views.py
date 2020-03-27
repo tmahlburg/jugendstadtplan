e946662c26c50ff01519b863fe9bb29b9368bee3
Till Mahlburg <mahlburg@posteo.de>
2020-03-27 19:12:20 +0100

    show all locations in the database with title popups

--- map/views.py
+++ map/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from .models import Location
+
 
 def index(request):
-    return render(request, 'map/index.html')
+    all_locations = Location.objects.all().values()
+    context = {'all_locations': all_locations,
+               'all_locations_length': all_locations.count()}
+    return render(request,
+                  'map/index.html',
+                  context)
