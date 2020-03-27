b0891cfa0479ce5afd26229c7b01ccaf1e5a9ad6
Till Mahlburg <mahlburg@posteo.de>
2020-03-27 19:15:47 +0100

    test todo added

--- map/views.py
+++ map/views.py
@@ -4,6 +4,7 @@ from .models import Location
 
 
 def index(request):
+    # TODO: tests
     all_locations = Location.objects.all().values()
     context = {'all_locations': all_locations,
                'all_locations_length': all_locations.count()}
