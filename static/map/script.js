63721442a53dc6a86834fa876cec9bdfcd844819
Till Mahlburg <mahlburg@posteo.de>
2020-04-17 00:36:09 +0200

    put all the map setup stuff in a function, which takes a viewpoint as argument

--- static/map/script.js
+++ static/map/script.js
@@ -1,21 +1,19 @@
+var map;
 
-var map = L.map('map', {zoomControl: false}).setView([54.095166, 13.3710154], 17);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+function setup_map(viewpoint = [54.095166, 13.3710154]) {
+    map = L.map('map', {zoomControl: false}).setView(viewpoint, 17);
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+	}).addTo(map);
+
+	L.control.zoom({position:'bottomleft'}).addTo(map);
+	map.on('click', onMapClick);
+}
 
 var popup = L.popup();
-
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
+        .setContent("Aktuelle Position: " + e.latlng.toString())
         .openOn(map);
 }
-
-L.control.zoom({
-     position:'bottomleft'
-}).addTo(map);
-
-map.on('click', onMapClick);
