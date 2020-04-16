8543b919e6958a6bc8ea206909f7afe4a373bfbd
Till Mahlburg <mahlburg@posteo.de>
2020-04-17 01:06:48 +0200

    make viewpoint setable via the URL

--- map/urls.py
+++ map/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    re_path(r'^(?P<viewpoint>\d+\.\d+\,\d+\.\d+)$',
+            views.index,
+            name='viewpoint')
 ]
--- map/views.py
+++ map/views.py
@@ -3,10 +3,25 @@ from django.shortcuts import render
 from .models import Location
 
 
-def index(request, viewpoint=[54.095166, 13.3710154]):
+def index(request, viewpoint='54.095166,13.3710154'):
     all_locations = Location.objects.all().values()
+    viewpoint = str_to_viewpoint(viewpoint)
     context = {'all_locations': all_locations,
                'viewpoint': viewpoint}
     return render(request,
                   'map/index.html',
                   context)
+
+
+def str_to_viewpoint(viewpoint):
+    """
+    Creates a viewpoint from a string.
+    :param viewpoint: Viewpoint as a string, formatted like this: 'lat,long'
+    :return: Viewpoint as list: [lat, long]
+    """
+    # split on ,
+    viewpoint = viewpoint.split(',')
+    # convert str to float
+    viewpoint = [float(coord) for coord in viewpoint]
+
+    return viewpoint
