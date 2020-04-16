c82d52b49b769e741de100bb21b5236d8d09ddcf
Till Mahlburg <mahlburg@posteo.de>
2020-04-17 00:36:55 +0200

    add viewpoint to page context

--- map/views.py
+++ map/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 from .models import Location
 
 
-def index(request):
+def index(request, viewpoint=[54.095166, 13.3710154]):
     all_locations = Location.objects.all().values()
-    context = {'all_locations': all_locations}
+    context = {'all_locations': all_locations,
+               'viewpoint': viewpoint}
     return render(request,
                   'map/index.html',
                   context)
