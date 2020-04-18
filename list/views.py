9afcde0959c7a95eca6b79d40d201c98463d5f21
Till Mahlburg <mahlburg@posteo.de>
2020-04-19 00:42:12 +0200

    add pagination

--- list/views.py
+++ list/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# understand data structure
 from map.models import Location
 
 
 def index(request):
-    all_locations = Location.objects.all().values()
-    context = {'all_locations': all_locations}
+    page = request.GET.get('page', 1)
+    location_list = Location.objects.all().values()
+    paginator = Paginator(location_list, 10)
+    try:
+        locations = paginator.page(page)
+    except PageNotAnInteger:
+        locations = paginator.page(1)
+    except EmptyPage:
+        locations = paginator.page(paginator.num_pages)
+
+    context = {'locations': locations}
     return render(request,
                   'list/index.html',
                   context)
