ddf49bc8fb5414212029ca610fed0fd139ea4e39
Till Mahlburg <mahlburg@posteo.de>
2020-04-21 02:24:58 +0200

    create basic form

--- suggestion/views.py
+++ suggestion/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from .forms import LocationForm
 
 
 def index(request):
     lat = request.GET.get('lat')
     lng = request.GET.get('lng')
+    form = LocationForm()
     context = {'lat': lat,
-               'lng': lng}
+               'lng': lng,
+               'form': form}
     return render(request,
                   'suggestion/index.html',
                   context)
