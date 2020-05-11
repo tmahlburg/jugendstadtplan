5f87aa61c324586c3d47d2c6e5a4ffafa2934a2c
Till Mahlburg <mahlburg@posteo.de>
2020-05-12 00:17:02 +0200

    register new model in admin page

--- map/admin.py
+++ map/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Location
+from .models import Location, LocationCategory
 
 admin.site.register(Location)
+admin.site.register(LocationCategory)
