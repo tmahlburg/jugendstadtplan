ef0549c9e05ea699d809c4abe1abba0c6f88daca
Till Mahlburg <mahlburg@posteo.de>
2020-05-19 01:21:46 +0200

    use tagging library

--- map/models.py
+++ map/models.py
@@ -1,30 +1,38 @@
 from django.db import models
+from tagging.fields import TagField
 
 
 class Location(models.Model):
     """
     Data model for the locations and places on the map.
     """
-    title = models.CharField(max_length=100, unique=True)
+    title = models.CharField(max_length=100, unique=True, verbose_name='Name')
 
     # location on the world using latitude (x) and longitude (y)
     latitude = models.FloatField()
     longitude = models.FloatField()
 
+    tags = TagField()
 
     # possible image
     picture = models.ImageField(upload_to='static/pictures/',
                                 null=True,
-                                blank=True)
+                                blank=True,
+                                verbose_name='Bild')
 
     # possible homepage
-    homepage = models.URLField(null=True, blank=True)
+    homepage = models.URLField(null=True,
+                               blank=True,
+                               verbose_name='Internetpräsenz')
 
     # possible address
-    address = models.TextField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True, verbose_name='Adresse')
 
     # possible opening hours, for now a simple text field
     # TODO: find a more sophisticated, semantically charged solution
-    opening_hours = models.TextField(null=True, blank=True)
+    opening_hours = models.TextField(null=True,
+                                     blank=True,
+                                     verbose_name='Öffnungszeiten')
 
-    is_public = models.BooleanField(default=False)
+    is_public = models.BooleanField(default=False,
+                                    verbose_name='Veröffentlicht')
