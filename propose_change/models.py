193f354d76b037a5eaf311bc3679d23bb547f450
Till Mahlburg <mahlburg [at] posteo.de>
2020-10-09 02:54:10 +0200

    define simple change proposal data model

--- propose_change/models.py
+++ propose_change/models.py
@@ -1,38 +1,13 @@
 from django.db import models
 from tagging.fields import TagField
 
+from map.models import Location
 
 class ChangeProposal(models.Model):
     """
-    Data model for the locations and places on the map. Change proposal edition.
+    Data model for proposals to change existing places.
     """
-    title = models.CharField(max_length=100, unique=True, verbose_name='Name')
 
-    # location on the world using latitude (x) and longitude (y)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    location_to_change = models.ForeignKey(Location, on_delete=models.CASCADE)
 
-    tags = TagField()
-
-    # possible image
-    picture = models.ImageField(upload_to='static/pictures/',
-                                null=True,
-                                blank=True,
-                                verbose_name='Bild')
-
-    # possible homepage
-    homepage = models.URLField(null=True,
-                               blank=True,
-                               verbose_name='Internetpräsenz')
-
-    # possible address
-    address = models.TextField(null=True, blank=True, verbose_name='Adresse')
-
-    # possible opening hours, for now a simple text field
-    # TODO: find a more sophisticated, semantically charged solution
-    opening_hours = models.TextField(null=True,
-                                     blank=True,
-                                     verbose_name='Öffnungszeiten')
-
-    is_public = models.BooleanField(default=False,
-                                    verbose_name='Veröffentlicht')
+    change_proposal = models.TextField(verbose_name='Was sollte hier anders sein?')
