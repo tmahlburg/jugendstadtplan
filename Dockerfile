0c6ad52c1eabb0cdc6ed730b5a5d1b7e442cce6e
Till Mahlburg <mahlburg@posteo.de>
2021-01-18 00:55:10 +0100

    initial dockerized production environment

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,12 @@
 # initial source: https://docs.docker.com/compose/django/
 
 FROM python:3-alpine
+ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
-RUN mkdir /code
-WORKDIR /code
-COPY requirements.txt /code/
+RUN mkdir /app
+WORKDIR /app
+RUN apk add zlib-dev jpeg-dev gcc musl-dev
+RUN pip install --upgrade pip
+COPY requirements.txt /app/
 RUN pip install -r requirements.txt
-COPY . /code/
+COPY . /app/
--- manage.sh
+++ manage.sh
@@ -20,9 +20,19 @@ setup_docker () {
 	python manage.py runserver 0.0.0.0:8000
 }
 
+# manages docker startup for production environments
+setup_prod () {
+	docker-compose down -v
+	docker-compose -f docker-compose.prod.yml down -v
+	docker-compose -f docker-compose.prod.yml up -d --build
+	docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput
+    docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
+}
+
 case "$1" in
 	setup_admin) setup_admin ;;
 	setup_docker) setup_docker ;;
+	setup_prod) setup_prod ;;
 	*) echo "Specify command: setup_admin or setup_docker"
 		return 1 ;;
 esac
