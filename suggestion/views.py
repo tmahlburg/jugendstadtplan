d2d4e7a2f79678cbe2864b72f78b0332f52908a8
Till Mahlburg <mahlburg@posteo.de>
2021-01-28 23:28:13 +0100

    add documentation and clean up code

--- suggestion/views.py
+++ suggestion/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+
 from .forms import LocationForm
-from map.models import Location
-from django.shortcuts import redirect
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
+    """
+    This returns a page with the form to suggest a new location, if the given
+    request is a 'GET', and it saves the given suggestion to the database and
+    returns to the map view, if the request is a 'POST'.
+
+    :param request: The HTTP request by which the view is requested.
+    :type request: HttpRequest
+    :return: The HTTP response with the suggestion page or the response
+             redirecting to the 'map' view.
+    :rtype: HttpResponse
+    """
     lat = request.GET.get('lat')
     lng = request.GET.get('lng')
     if request.method == 'POST':
@@ -12,13 +23,13 @@ def index(request):
         if form.is_valid():
             form.save()
         return redirect('/map')
-    else:
-        form = LocationForm()
-        form.fields['latitude'].initial = lat
-        form.fields['longitude'].initial = lng
-        context = {'lat': lat,
-                   'lng': lng,
-                   'form': form}
+    # if the request is a 'GET'
+    form = LocationForm()
+    form.fields['latitude'].initial = lat
+    form.fields['longitude'].initial = lng
+    context = {'lat': lat,
+               'lng': lng,
+               'form': form}
     return render(request,
                   'suggestion/index.html',
                   context)
