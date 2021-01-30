7e80874e308d395835d05c84fb050766d9b9fe84
Till Mahlburg <mahlburg@posteo.de>
2021-01-30 01:19:01 +0100

    clean up js a bit

--- staticfiles/map/script.js
+++ staticfiles/map/script.js
@@ -1,13 +1,12 @@
 var map;
-
 function setup_map(viewpoint = [54.08950301403954, 13.40512275695801], zoom = 14) {
     map = L.map('map', {zoomControl: false}).setView(viewpoint, zoom);
-	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
+    }).addTo(map);
 
-	L.control.zoom({position:'bottomright'}).addTo(map);
-	map.on('click', onMapClick);
+    L.control.zoom({position:'bottomright'}).addTo(map);
+    map.on('click', onMapClick);
 }
 
 var popup = L.popup();
@@ -18,6 +17,6 @@ function onMapClick(e) {
         .openOn(map);
 }
 
-function close_toast(btn){
-	((btn.parentNode).parentNode).removeChild(btn.parentNode);
+function close_toast(btn) {
+    ((btn.parentNode).parentNode).removeChild(btn.parentNode);
 }
--- staticfiles/script.js
+++ staticfiles/script.js
@@ -1,24 +1,31 @@
-// This file contains js used by multiple apps
+// This	file contains js used by multiple apps
 function reload_with_filter(tags, small) {
-	var params = "";
-	var suffix = "";
+	var	params = "";
+	var	suffix = "";
 	if (small) {
 		suffix = "-small";
 	}
-	for (var i = 0; i < tags.length; i++) {
-		var tag_checkbox = document.getElementById(tags[i].name + suffix);
+	for	(var i = 0;	i <	tags.length; i++) {
+		var	tag_checkbox = document.getElementById(tags[i].name	+ suffix);
 		if (tag_checkbox.checked) {
 			params += tags[i].name + ",";
 		}
 	}
 	if (params != "") {
 		params = params.slice(0,-1);
-		var url = window.location.href;
-		url = url.split('?')[0];
-		url = url + '?tags=' + params;
+		var	url	= window.location.href;
+		url	= url.split('?')[0];
+		url	= url +	'?tags=' + params;
 		window.location.href = url;
 	} else {
-		var warning = document.getElementById("empty-tags-warning");
+		var	warning	= document.getElementById("empty-tags-warning");
 		warning.removeAttribute("hidden");
 	}
 }
+
+function close_modal_and_reload()	{
+	document.getElementById("modal").classList.remove("active");
+	tags_json =	JSON.parse("{{ tags_json|escapejs }}");
+	reload_with_filter(tags_json, true);
+}
+
