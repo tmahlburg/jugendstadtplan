0bc6a61505832eca28582b9ca41bb5e0eda669c0
Till Mahlburg <mahlburg@posteo.de>
2021-03-21 00:17:28 +0100

    add to suggestion page

--- suggestion/forms.py
+++ suggestion/forms.py
@@ -6,7 +6,7 @@ class LocationForm(forms.ModelForm):
     class Meta:
         model = Location
         fields = ('title', 'latitude', 'longitude', 'tags', 'description', 'picture',
-                  'homepage', 'facebook', 'instagram', 'tiktok', 'twitter', 'address', 'opening_hours', 'is_on_placem')
+                  'homepage', 'facebook', 'instagram', 'tiktok', 'twitter', 'address', 'opening_hours', 'is_on_placem', 'by_or_for')
         help_texts = {
             'tags': ('Hier kannst du Tags eintragen, die den Ort einordnen. Willst du ein Kino eintragen? Schreib "kino". Ist der Ort ein Treffpunkt? Schreib "treff". Kann man dort außerdem auch Musik machen? Schreib "treff musik". Tags sollten vollständig klein geschrieben werden und mehrere Tags werden durch Leerzeichen getrennt.')
        }
