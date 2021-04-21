3029dd60983dc5db588b60fd7140a5f688314f3a
Till Mahlburg <mahlburg@posteo.de>
2021-04-21 13:01:02 +0200

    initial functionality

--- list/views.py
+++ list/views.py
@@ -20,13 +20,27 @@ def index(request: HttpRequest) -> HttpResponse:
     :rtype: HttpResponse
     """
     recieved_tags = request.GET.get('tags')
+    rcv_by_or_for = request.GET.get('by_or_for')
     page = request.GET.get('page', 1)
-    if (recieved_tags):
+    if recieved_tags:
         location_list = get_locations_from_tags(recieved_tags)
         tags = build_tag_list(Location.objects.all().values(), recieved_tags)
     else:
         location_list = Location.objects.order_by('title').values()
         tags = build_tag_list(location_list, recieved_tags)
+
+    by_or_for = {'by': True,
+                 'for': True}
+
+    if rcv_by_or_for:
+       by_or_for = {'by': False,
+                    'for': False}
+       rcv_by_or_for = rcv_by_or_for.split(',')
+       for element in rcv_by_or_for:
+           if element == 'by':
+               by_or_for['by'] = True;
+           elif element == 'for':
+               by_or_for['for'] = True;
 
     paginator = Paginator(location_list, 10)
     try:
@@ -42,6 +56,7 @@ def index(request: HttpRequest) -> HttpResponse:
                'there_are_public_locations':
                    there_are_public_locations(location_list),
                'tags': tags,
+               'by_or_for': by_or_for,
                'tags_json': tags_json}
     return render(request,
                   'list/index.html',
